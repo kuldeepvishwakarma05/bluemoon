51c7d7052a26ced4e618272b0141ce5526b86817
kuldeep vishwakarma <kv752547@gmail.com>
2020-10-26 13:00:40 +0530

    Updates

---
+++ precache-manifest.9b6a85a38c5d02fec470b6f552109e6d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3fda98000a773ae2568749c7053a0a",
+    "url": "/bluemoon/index.html"
+  },
+  {
+    "revision": "9988da31430d2304602c",
+    "url": "/bluemoon/static/css/main.5e867ac3.chunk.css"
+  },
+  {
+    "revision": "c3475fa1af6f958225e9",
+    "url": "/bluemoon/static/js/2.71b937d1.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/bluemoon/static/js/2.71b937d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9988da31430d2304602c",
+    "url": "/bluemoon/static/js/main.3fe6a1b9.chunk.js"
+  },
+  {
+    "revision": "64110d92178abe34b3a5",
+    "url": "/bluemoon/static/js/runtime-main.ec47447f.js"
+  },
+  {
+    "revision": "60ee2b2ead2f201143f611e73c9b8bce",
+    "url": "/bluemoon/static/media/card1.60ee2b2e.jpg"
+  },
+  {
+    "revision": "3dc769562a40a99cfb6cf79609d0d5ca",
+    "url": "/bluemoon/static/media/card2.3dc76956.jpg"
+  },
+  {
+    "revision": "4ce868f3f8c04ca283d99b9208440b49",
+    "url": "/bluemoon/static/media/card3.4ce868f3.jpg"
+  },
+  {
+    "revision": "e9a954df09c53a2c72a72cd01a3232fd",
+    "url": "/bluemoon/static/media/card4.e9a954df.jpg"
+  },
+  {
+    "revision": "43591eda253900caca8bc0cc05c07b8e",
+    "url": "/bluemoon/static/media/card5.43591eda.jpg"
+  },
+  {
+    "revision": "d66e56547162f010adad85e1023fc7ec",
+    "url": "/bluemoon/static/media/card6.d66e5654.jpg"
+  },
+  {
+    "revision": "fe863a887f0536135033b265b6e91d8e",
+    "url": "/bluemoon/static/media/card7.fe863a88.jpg"
+  },
+  {
+    "revision": "3c7e2777d30d0b291a25045f605cc0bc",
+    "url": "/bluemoon/static/media/card8.3c7e2777.jpg"
+  },
+  {
+    "revision": "a01a1dcec345039709cd0936959d3af6",
+    "url": "/bluemoon/static/media/dev.a01a1dce.jpg"
+  },
+  {
+    "revision": "8c3788072529e85df0cbb468406c7313",
+    "url": "/bluemoon/static/media/homebg.8c378807.jpg"
+  },
+  {
+    "revision": "36daaa9fc1bdc7c3a4c29a162329d67c",
+    "url": "/bluemoon/static/media/khushboo.36daaa9f.jpg"
+  },
+  {
+    "revision": "461f305d9c48d3dca1fe86b03fd7a76e",
+    "url": "/bluemoon/static/media/sarvesh.461f305d.jpg"
+  },
+  {
+    "revision": "f97a863256f5364c187c599c6868c5e2",
+    "url": "/bluemoon/static/media/ved.f97a8632.jpg"
+  }
+]);
